c9c23f47b92a29a25adf88bc6b541d7f7c1fd6a1
firstcommit <rosenich@badinhs.org>
2016-10-06 08:26:26 -0400

    Functions and Array created

--- SwiftTableViewApp/ViewController.swift
+++ SwiftTableViewApp/ViewController.swift
@@ -8,12 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
 
+    @IBOutlet weak var coolTable: UITableView!
+    
+    var lunchItems=["badin bowl","bosco sticks","cheese burger","chicken nuggets","chicken wrap wuth lettuce and cheese","chili cheese coney","fish sandwich","fries","hotdog","pizza"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return lunchItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(indexPath.row)
+        let cell=UITableViewCell()
+        cell.textLabel?.text=lunchItems[indexPath.row]
+        return cell
+    }
+    
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
